cc0f82072fe16ffa69a9cfe15de44cf8a2689c28
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-01-23 13:10:59 -0500

    routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+router.get('/api/notes', (req, res) => {
+    res.json(notesData)
+});
+
+// POST /api/notes should receive a new note to save on the request body,
+// add it to the db.json file, and then return the new note to the client.
+// You'll need to find a way to give each note a unique id when it's saved
+// (look into npm packages that could do this for you).
+router.post('/api/notes', (req, res) => {
+    if(notesData.length == 0) {
+        req.body.id = '0';
+    } else {
+        req.body.id = JSON.stringify(JSON.parse(notesData[notesData.length - 1].id) + 1);
+    }
+    console.log('req.body.id:' + req.body.id);
+
+    // pushes
+
+});
+
+// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete.
+// In order to delete a note, you'll need to read all notes from the db.json file,
+// remove the note with the given id property, and then rewrite the notes to the db.json file.
+
+router.delete((re, res) => {
+
+})
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require('express');
+const path = require('path');
 
 // sets port
 const PORT = process.env.PORT || 3001;
 // initiate express server
 const app = express();
+// connect routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-const db = require('./db/db.json');
+// use api routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
+// middleware
+// connects css, js & html
+app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-
-
-app.get('/api/db', (req, res) => {
-    res.json(db)
-});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
